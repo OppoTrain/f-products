b4587ffee9d5a3e8e506d02b903e7cdd066d187d
rama <ramahahawash2003>
2024-11-07 14:05:27 +0200

    languages

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import bella1 from '../../assets/hero2 .png'
-import bella2 from '../../assets/hero4.jpg'
-import './Slider.css';
-import Product from '../product/Product';
+import React from "react";
+import bella1 from "../../assets/hero2 .png";
+import bella2 from "../../assets/hero4.jpg";
+import { useTranslation } from "react-i18next";
+import "./Slider.css";
 
 export default function Slider() {
+  const { t } = useTranslation();
+
   return (
     <>
-      <div  className="carousel slide "   data-bs-ride="carousel" data-bs-interval={3000}>
+      <div
+        className="carousel slide"
+        data-bs-ride="carousel"
+        data-bs-interval={3000}
+      >
         <div className="carousel-inner">
- 
           <div className="carousel-item active">
-            <img src={bella1} className="d-block w-100" alt="Pastel Blue Image" />
-            <h3 className="carousel-caption">You Can Be Yourself Every Day</h3>
+            <img
+              src={bella1}
+              className="d-block w-100"
+              alt="Pastel Blue Image"
+            />
+            <h3 className="carousel-caption">{t("slider.caption1")}</h3>
           </div>
 
-          <div className="carousel-item " >
-            <img src={bella2} className="d-block w-100" alt="Pastel Peach Image" />
-            <h3 className="carousel-caption">For Each of Us Just Bella</h3>
+          <div className="carousel-item">
+            <img
+              src={bella2}
+              className="d-block w-100"
+              alt="Pastel Peach Image"
+            />
+            <h3 className="carousel-caption">{t("slider.caption2")}</h3>
           </div>
-
         </div>
-      </div>
-
-      <div className="product-section">
-        <Product />
       </div>
     </>
   );
